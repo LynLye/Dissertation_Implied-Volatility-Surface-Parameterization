63fa391a0399da0e0f85a77d98fed2affffd82f9
Lyn <yelin_lyn@163.com>
2017-11-15 21:04:52 +0800

    updata set_up

--- setup.py
+++ setup.py
@@ -6,14 +6,20 @@ Created on Wed Nov 15 16:55:38 2017
 @author: lyn
 """
 
-from distutils.core import setup
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
 
 
 setup(
      name='py_implied_vol',
-     version='0.1',
+     version='0.1.1',
+     description='Python implied vol parameterization based on SVI and SABR',
      author='Ye,Lin',
      author_email='yelin_lyn@163.com',
-          url='',
-     download_url='',
+     url='https://github.com/LynLye/Dissertation_Implied-Volatility-Surface-Parameterization',
+    package_dir={'Dissertation_Implied-Volatility-Surface-Parameterization':'py_implied_vol'},
+    include_package_data=True,
      )
